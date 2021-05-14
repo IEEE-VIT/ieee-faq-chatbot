129475dbee8481b07c041b150b907fd1bc6a251d
Guru Aathavan AL U <58874818+guruaathavanalu@users.noreply.github.com>
2021-05-14 18:09:26 +0530

    Add files via upload

---
+++ chatbot_pytorch_fastapi/app.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 17:37:18 2021
+
+@author: Guru Aathavan AL U
+"""
+
+from fastapi import FastAPI
+import uvicorn
+import time
+import conversions
+import model
+
+app=FastAPI()
+
+@app.get("/")
+def index():
+    return {"IEEE Chatbot": "v2"}
+
+@app.get("/trail")
+def get_name(name:str):
+    return{'trail':f'{name}'}
+
+@app.post('/predict')
+def predict_intent(data:str):
+    res_time=time.time()
+    query=data
+    data=conversions.convert(data)    
+    """
+    hidden=val_h
+    _,output=torch.topk(net_1(data,hidden)[0],1)
+    label=int_to_label[output.item()]
+    output=label_to_response[int_to_label[output.item()]]
+    """
+    label,output=model.custom_forward(data)
+    
+    res_time=time.time()-res_time
+    return{
+        'query':query,
+        'label':label,
+        'response':output,
+        'response time on gpu':res_time
+        }
+
+if __name__=="main":
+    uvicorn.run(app,host='127.0.0.1',port=8000)
+    
---
+++ chatbot_pytorch_fastapi/conversions.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 17:17:39 2021
+
+@author: Guru Aathavan AL U
+"""
+
+import pickle
+import numpy as np
+import torch
+
+def pad_features(reviews_ints, seq_length=20):
+    # getting the correct rows x cols shape
+    features = np.zeros((len(reviews_ints), seq_length), dtype=int)
+    # for each review, I grab that review and 
+    for i, row in enumerate(reviews_ints):
+        features[i, -len(row):] = np.array(row)[:seq_length]
+    return features
+
+#all dictionaries for word to int and vice versa conversion
+
+f=open("word_to_int.pkl","rb")
+word_to_int=pickle.load(f)
+
+f=open("int_to_word.pkl","rb")
+int_to_word=pickle.load(f)
+
+f=open("label_to_int.pkl","rb")
+label_to_int=pickle.load(f)
+
+f=open("int_to_label.pkl","rb")
+in_to_label=pickle.load(f)
+
+f=open("label_to_response.pkl","rb")
+labe_to_response=pickle.load(f)
+
+
+def convert(question):
+    question=question.split()
+    q=[]
+    for i in question:
+        q.append(word_to_int[i])
+    q=np.array(q)
+    print(q)
+    inputs=torch.Tensor(pad_features([q])).cuda()
+    inputs=torch.tensor(inputs).to(torch.int64)
+    return inputs
+
+def int_to_label(num):
+    return (in_to_label[num])
+
+def label_to_response(label):
+    return (labe_to_response[label])
