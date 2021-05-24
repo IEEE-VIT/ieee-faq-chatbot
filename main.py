56a2da6744355ee2e4bee13a1b4739fc32cebf34
Harsh Malani <58773999+Harsh-1309@users.noreply.github.com>
2021-05-24 14:25:13 +0530

    Added option to use both models

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import uvicorn
+import time
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 from user_input import user_input
-from model import ieee_faq_bot
+from tensorflow_model import ieee_faq_bot
 from user_output import user_output
+
+def tensorflow_model(user_query):
+    # classify the input query as one of the intents using tensorflow
+    make_prediction = ieee_faq_bot(user_query)
+    return make_prediction 
+
+def pytorch_model(user_query):
+    # classify the input query as one of the intents using pytorch
+    make_prediction = "gen_bye"
+    return make_prediction
 
 # create app object with title and description 
 app = FastAPI(
@@ -11,23 +23,41 @@ app = FastAPI(
 
 @app.get('/')
 def index():
-    return {"message": "IEEE FAQ chatbot is live"}
+    message = "IEEE FAQ chatbot is live. Head over to http://127.0.0.1:8000/docs to test it out."
+    return message
 
-@app.post('/chatbot')
+@app.post('/chatbot',description="enter 1 under model_selection for tensorflow model and 2 for pytorch model")
 def chatbot(data: user_input):
+    res_time=time.time()
     data = data.dict()
-    # load input query by user
+
+    # load input query and model by user
     user_query = data['query']  
-    # classify the input query as one of the intents
-    make_prediction = ieee_faq_bot(user_query)
-    # get output based on classified intent
-    final_user_output = user_output(make_prediction)
+    selected_model = data['model_selection']
+
+    if selected_model == 1:
+        predicted_intent=tensorflow_model(user_query)
+        model_in_use = "TensorFlow"
+    elif selected_model == 2:
+        predicted_intent=pytorch_model(user_query)
+        model_in_use = "PyTorch"
+    else :
+        raise Exception("Please enter a valid model")
+
+    #generate final output for given query
+    final_user_output = user_output(predicted_intent)
 
     return {  
         'user query': user_query,
-        'prediction': make_prediction,
-        'output to user': final_user_output
+        'predicted intent': predicted_intent,
+        'output to user': final_user_output,
+        'model used' : model_in_use,
+        'response time' : res_time
     }
 
+@app.exception_handler(Exception)
+async def validation_exception_handler(request,exc):
+    return PlainTextResponse("Enter a valid model. 1 for tensorflow and 2 for pytorch", status_code=400)
+
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000, Reload=True)
